cd1bd83df7ebf5f816022c70730587b8cbae2275
SilviaAriza <silvia01ariza@gmail.com>
2025-11-25 07:34:40 -0500

    :3

--- app/src/main/java/com/example/netflixapp/VideoPromocionalActivity.java
+++ app/src/main/java/com/example/netflixapp/VideoPromocionalActivity.java
@@ -12,19 +12,5 @@ public class VideoPromocionalActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video_promocional);
-
-        WebView webView = findViewById(R.id.webViewVideo);
-
-        WebSettings webSettings = webView.getSettings();
-        webSettings.setJavaScriptEnabled(true);
-
-        webView.setWebViewClient(new WebViewClient());
-
-        // Inserta aquí tu enlace de YouTube
-        String videoHTML = "<iframe width=\"100%\" height=\"100%\" "
-                + "src=\"https://www.youtube.com/embed/dQw4w9WgXcQ\" "
-                + "frameborder=\"0\" allowfullscreen></iframe>";
-
-        webView.loadData(videoHTML, "text/html", "utf-8");
     }
 }
